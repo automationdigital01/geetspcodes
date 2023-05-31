0c115138a137c97ca6e4543e12a2f3b8fc8a24bb
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-05-31 18:05:27 +0530

    Update CV_parsing.py

--- CV_parsing.py
+++ CV_parsing.py
@@ -71,7 +71,7 @@ def predict(filepaths, nlp, skills):
     entities_list = []
 
     for filepath in filepaths:
-        file_contents = filepath.read().decode('utf-8')
+        file_contents = filepath.read().decode('utf-8').strip()
         email = None
         name = None
         roles = None
