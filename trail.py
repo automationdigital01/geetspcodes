37f85ac504b4240f6fee7f13eb5d84be61f67ecf
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-06-01 16:08:50 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -6,7 +6,7 @@ uploaded_files = st.file_uploader("Upload PDF file", accept_multiple_files=True)
 # Check if a file has been uploaded
 if uploaded_files is not None:
     for file in uploaded_files:
-        with open(, 'rb') as file:
+        with open(file , 'rb') as file:
             read=PyPDF2.PdfReader(file)
             pagenum=len(read.pages)
             page_content=read.pages[0]
