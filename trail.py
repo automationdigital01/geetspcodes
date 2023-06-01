c3c71e54d1be8869a065854579dade9fe986ee81
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-06-01 16:27:39 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -2,12 +2,10 @@ import streamlit as st
 import PyPDF2
 # Display a file uploader widget
 uploaded_files = st.file_uploader("Upload PDF file", accept_multiple_files=True, type="pdf")
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-# Check if a file has been uploaded
+
+    # Check if a file has been uploaded
 if uploaded_files is not None:
     for file in uploaded_files:
-        show_pdf(file)
+        base64_pdf = base64.b64encode(file.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
