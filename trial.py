6abebd1a4b0193ce5b2ea7b7e71b5db16af7dcde
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-06-02 12:03:46 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,8 +1,17 @@
 import streamlit as st
-import PyPDF2
+#import PyPDF2
+from PyPDF2 import PdfFileReader 
 # Display a file uploader widget
 uploaded_files = st.file_uploader("Upload PDF file", accept_multiple_files=True, type="pdf")
+def read_pdf(file):
+	pdfReader = PdfFileReader(file)
+	count = pdfReader.numPages
+	all_page_text = ""
+	for i in range(count):
+		page = pdfReader.getPage(i)
+		all_page_text += page.extractText()
 
+	return all_page_text
 
 # Check if files have been uploaded
 if uploaded_files:
@@ -11,7 +20,7 @@ if uploaded_files:
     # Iterate through the uploaded files
     for uploaded_file in uploaded_files:
         # Read the file contents
-        file_contents = uploaded_file.read()
+        file_contents = read_pdf(uploaded_file)
 
         # Display the file name and contents
         st.write("File name:", uploaded_file.name)
