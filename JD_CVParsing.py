fbff9e416f4f972f25d146116ea4b330dfccf96d
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-06-26 14:01:24 +0530

    Update JD_CVParsing.py

--- JD_CVParsing.py
+++ JD_CVParsing.py
@@ -267,7 +267,8 @@ def main():
                
                match_file.append(cv_file)
                match_percent.append(Match)
-       df = predict(match_file, nlp)        
+       for f in match_file:
+           df = predict(match_file, nlp)        
        df=df.assign(File=[match_file])
        df=df.assign(Match_percent=[match_percent])
        df=df.astype(str)
