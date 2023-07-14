856c7a9164ce3dd5e40d4cdadfb7ac34ef93130b
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-07-14 10:58:12 +0530

    Update text_extract.py

--- text_extract.py
+++ text_extract.py
@@ -1,16 +1,26 @@
 import cv2
 from pytesseract import Output
+import streamlit as st
+from PIL import Image
+#load image
+st.title("Text Extract")
+image= st.uploader("Choose a file")
+st.image(image, caption= "loaded image")
 
-img = cv2.imread(r'C:\Users\gpandey2\Desktop\FMC files\CD2 - Well Cleanup Datasheet_page-0001.jpg')
-d = pytesseract.image_to_data(img, output_type=Output.DICT)
-keys = list(d.keys())
-
-data_pattern = 'FLOPETROL'
-
-n_boxes = len(d['text'])
-for i in range(n_boxes):
-    if float(d['conf'][i]) > 60:
-        if re.match(data_pattern, d['text'][i]):
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#giving options to extract whole text content or particular text
+with col1:
+    option= st.radio("How you want the output, please choose from below :",
+             key="visibility",
+             options= ["Extract whole text","Extract partcular text"])
+    
+if option=="Extract partcular text":
+    text=st.text_input("text to be searched", "")
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    keys = list(d.keys())
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if float(d['conf'][i]) > 60:
+            if re.match(data_pattern, d['text'][i]):
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 Image.fromarray(img)
